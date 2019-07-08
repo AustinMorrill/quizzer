d0d717f8f65d29448af892a4f10fe42315fb1506
AustinMorrill <austin.r.morrill@gmail.com>
2019-07-08 13:06:39 -0600

    heroku done

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express")
 const app = express()
 require("dotenv").config()
-const morgan = require("morgan")
 const mongoose = require("mongoose")
 const expressJwt = require("express-jwt")
 const PORT = process.env.PORT || 5000
-app.use(morgan("dev"))
+const path = require("path")
+
+
 app.use(express.json())
 app.use("/api", expressJwt({ secret: process.env.SECRET }))
+app.use(express.static(path.join(__dirname, "client", "build")))
+app.use("/auth", require("./routes/auth"))
+app.use("/api/stat", require("./routes/stats"))
+app.use((err, req, res, next) => {
+	console.error(err)
+	if (err.name === "UnauthorizedError") {
+		res.status(err.status)
+	}
+	return res.send({ message: err.message })
+})
 
 //connect to db
 mongoose.set("useCreateIndex", true)
@@ -16,9 +27,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/quizzer",
 	console.log("Connected to the database")
 })
 
-app.use("/auth", require("./routes/auth"))
-app.use("/api/stat", require("./routes/stats"))
-
 app.use((err, req, res, next) => {
 	console.error(err)
 	if (err.name === "UnauthorizedError") {
@@ -26,6 +34,7 @@ app.use((err, req, res, next) => {
 	}
 	return res.send({ message: err.message })
 })
+
 
 app.listen(PORT, () => {
 	console.log(`[+] Starting server on port ${PORT}`)
